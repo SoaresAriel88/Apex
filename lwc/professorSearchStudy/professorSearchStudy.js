fd00b094a2002aa609eed6358e5572400db16512
ArielSoares <124395292+SoaresAriel88@users.noreply.github.com>
2025-09-15 17:11:35 -0300

    Update professorSearchStudy.js

--- lwc/professorSearchStudy/professorSearchStudy.js
+++ lwc/professorSearchStudy/professorSearchStudy.js
@@ -1,32 +1,63 @@
-import { LightningElement, wire } from 'lwc';
+import {LightningElement, wire, track} from 'lwc';
 import searchStudyAndCurso from '@salesforce/apex/SearchTeacher.searchStudyAndCurso';
-export default class ProfessorSearchStudy extends LightningElement {
+import searchTeacherName from '@salesforce/apex/SearchTeacher.searchTeacherName';
 
-searchAluno = '';
-searchCurso = '';
-     
-    @wire(searchStudyAndCurso, { professor: '$searchAluno' }) searchTeacher;
 
+const columns = [
+    { label: 'Aluno', fieldName: 'Name'},
+    { label: 'Curso', fieldName: 'CursoName'}
+];
+
+
+export default class ProfessorSearchStudy2 extends LightningElement {
+
+
+columns = columns;
+showTable = false;
+searchTeacherData;
+searchTeacherNameId;
+
+@track teacherNameOptions = [];
+
+//PICKLIST/METODO
  
- 
- 
- 
- 
- 
- 
- 
-    get optionsAlunosAndCursos() {
-        return [
-            { label: 'Clemilton', value: 'Clemilton' },
-            { label: 'Ativo', value: 'Ativo' },
-            { label: 'Inativo', value: 'Inativo'},
-        ];
+handleChangeAlunosAndCursos(event) {
+        this.searchTeacherNameId = event.detail.value;
+        this.showTable= true;
     }
 
-    handleChangeAlunosAndCursos(event) {
-        this.searchAlunosAndCursos = event.detail.value;
-     
+//CHAMADA DOS MÉTODOS
+
+@wire(searchTeacherName)
+    wiredTeachers({data, error}){
+        console.log('Lista dos IDS: ' + JSON.stringify(data));
+        if(data){
+            this.teacherNameOptions = data.map( teacher => ({ label: teacher.Name, value: teacher.Id}));
+            
+        }else if(error){
+            console.log(error);
+
+        }
     }
 
 
-}
+
+@wire(searchStudyAndCurso, { professorId: '$searchTeacherNameId' }) 
+    wiredStudyAndCursos({data, error}) {
+        console.log('Lista de Alunos e Cursos: ' + JSON.stringify(data));
+        if (data) {
+            this.searchTeacherData = data?.map((item) => {
+                return {
+                    Name: item.Name,
+                    CursoName: item.Curso__r?.Name
+                }
+            });
+        } else if (error) {
+            console.error(error);
+        }
+    }
+ 
+   // OBJECT DESTRUCTURING
+    //FUNCTION DESTRUCTURING
+    //MAP FUNCTION JS
+ }
